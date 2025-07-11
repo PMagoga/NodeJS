305fed5f01443aa58b14f2ee8b94f4f434f54404
PMagoga <pmagoga@gmail.com>
2025-07-11 15:30:04 -0300

    projetos em typescript

---
+++ typescript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function myEmail() {
+    let myEmail = 'pmagoga@gmail.com';
+    console.log(myEmail);
+}
+myEmail();
---
+++ typescript/src/index.ts
@@ -0,0 +1,6 @@
+function myEmail() {
+  let myEmail = 'pmagoga@gmail.com';
+  console.log(myEmail);
+}
+
+myEmail();
