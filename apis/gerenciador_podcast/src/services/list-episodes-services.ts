933f5f78a1152d82c84001ef8d22ab47bb23069b
Pablo <94563183+PMagoga@users.noreply.github.com>
2025-07-15 14:32:45 -0300

    Delete apis/gerenciador_podcast/src directory

--- apis/gerenciador_podcast/src/services/list-episodes-services.ts
+++
@@ -1,7 +0,0 @@
-import { repositoryPodcast } from "../repositories/podcast-reposity";
-
-export const serviceListEpisodes = async () => {
-    const data = await repositoryPodcast();    
-
-    return data;
-};
