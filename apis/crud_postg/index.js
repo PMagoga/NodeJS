a4fbf75c2f1febea755bb11a764dd3349ed1688a
PMagoga <pmagoga@gmail.com>
2025-10-17 12:33:03 -0300

    excluindo pasta scrimba

---
+++ apis/crud_postg/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+
+const db = require("./db");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
